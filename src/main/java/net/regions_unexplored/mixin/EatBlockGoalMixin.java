91f6e091d5ff15ebccc1cd13142962927afc2147
JoostMSoftware <joostmsoftware@gmail.com>
2024-02-22 10:38:56 +0100

    Goodbye dodgy mixin, should fix issue #80 and #93

--- src/main/java/net/regions_unexplored/mixin/EatBlockGoalMixin.java
+++ src/main/java/net/regions_unexplored/mixin/EatBlockGoalMixin.java
@@ -1,9 +1,11 @@
 package net.regions_unexplored.mixin;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.goal.EatBlockGoal;
 import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.level.GameRules;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
@@ -14,59 +16,64 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.function.Predicate;
 
-@Mixin(EatBlockGoal.class)
-public class EatBlockGoalMixin extends Goal {
+@Mixin(value = EatBlockGoal.class)
+public class EatBlockGoalMixin {
     @Shadow
     @Final
-    private static Predicate<BlockState> IS_TALL_GRASS;
+    public Mob mob;
 
-    @Inject(at=@At("TAIL"), method="tick()V")
-    private void tick(CallbackInfo ci) {
-        if (((EatBlockGoal)(Object)this).getEatAnimationTick() == this.adjustedTickDelay(4)) {
-            BlockPos blockpos = ((EatBlockGoal)(Object)this).mob.blockPosition();
-            BlockPos blockpos1 = blockpos.below();
-            if (((EatBlockGoal)(Object)this).level.getBlockState(blockpos1).is(RuBlocks.PEAT_GRASS_BLOCK.get())) {
-                if (((EatBlockGoal)(Object)this).level.getGameRules().getBoolean(GameRules.RULE_MOBGRIEFING)) {
-                    ((EatBlockGoal)(Object)this).level.levelEvent(2001, blockpos1, Block.getId(RuBlocks.PEAT_GRASS_BLOCK.get().defaultBlockState()));
-                    ((EatBlockGoal)(Object)this).level.setBlock(blockpos1, RuBlocks.PEAT_DIRT.get().defaultBlockState(), 2);
-                }
-                ((EatBlockGoal)(Object)this).mob.ate();
-            } else if (((EatBlockGoal)(Object)this).level.getBlockState(blockpos1).is(RuBlocks.SILT_GRASS_BLOCK.get())) {
-                if (((EatBlockGoal)(Object)this).level.getGameRules().getBoolean(GameRules.RULE_MOBGRIEFING)) {
-                    ((EatBlockGoal)(Object)this).level.levelEvent(2001, blockpos1, Block.getId(RuBlocks.SILT_GRASS_BLOCK.get().defaultBlockState()));
-                    ((EatBlockGoal)(Object)this).level.setBlock(blockpos1, RuBlocks.SILT_DIRT.get().defaultBlockState(), 2);
-                }
-                ((EatBlockGoal)(Object)this).mob.ate();
-            } else if (((EatBlockGoal)(Object)this).level.getBlockState(blockpos1).is(RuBlocks.ALPHA_GRASS_BLOCK.get())) {
-                if (((EatBlockGoal)(Object)this).level.getGameRules().getBoolean(GameRules.RULE_MOBGRIEFING)) {
-                    ((EatBlockGoal)(Object)this).level.levelEvent(2001, blockpos1, Block.getId(RuBlocks.ALPHA_GRASS_BLOCK.get().defaultBlockState()));
-                    ((EatBlockGoal)(Object)this).level.setBlock(blockpos1, Blocks.DIRT.defaultBlockState(), 2);
-                }
-                ((EatBlockGoal)(Object)this).mob.ate();
-            }
+    @Shadow
+    @Final
+    public Level level;
+
+    @Inject(method = "canUse",
+            at = @At(value = "FIELD", target = "Lnet/minecraft/world/level/block/Blocks;GRASS_BLOCK:Lnet/minecraft/world/level/block/Block;"),
+            cancellable = true,
+            locals = LocalCapture.NO_CAPTURE
+    )
+    private void regions_unexplored$canUse(CallbackInfoReturnable<Boolean> cir) {
+        BlockPos pos = this.mob.blockPosition();
+
+        if (this.level.getBlockState(pos.below()).is(RuBlocks.SILT_GRASS_BLOCK.get()) || this.level.getBlockState(pos.below()).is(RuBlocks.PEAT_GRASS_BLOCK.get())) {
+            cir.setReturnValue(true);
         }
     }
 
-    @Override
-    public boolean canUse() {
-        if (((EatBlockGoal) (Object) this).mob.getRandom().nextInt(((EatBlockGoal) (Object) this).mob.isBaby() ? 50 : 1000) != 0) {
-            return false;
-        } else {
-            BlockPos blockPos = ((EatBlockGoal) (Object) this).mob.blockPosition();
-            if (IS_TALL_GRASS.test(((EatBlockGoal) (Object) this).level.getBlockState(blockPos))) {
-                return true;
-            } else if (((EatBlockGoal) (Object) this).level.getBlockState(blockPos.below()).is(Blocks.GRASS_BLOCK)) {
-                return ((EatBlockGoal) (Object) this).level.getBlockState(blockPos.below()).is(Blocks.GRASS_BLOCK);
-            } else if (((EatBlockGoal) (Object) this).level.getBlockState(blockPos.below()).is(RuBlocks.SILT_GRASS_BLOCK.get())) {
-                return ((EatBlockGoal) (Object) this).level.getBlockState(blockPos.below()).is(RuBlocks.SILT_GRASS_BLOCK.get());
-            } else if (((EatBlockGoal) (Object) this).level.getBlockState(blockPos.below()).is(RuBlocks.ALPHA_GRASS_BLOCK.get())) {
-                return ((EatBlockGoal) (Object) this).level.getBlockState(blockPos.below()).is(RuBlocks.ALPHA_GRASS_BLOCK.get());
-            } else {
-                return ((EatBlockGoal) (Object) this).level.getBlockState(blockPos.below()).is(RuBlocks.PEAT_GRASS_BLOCK.get());
+    @Inject(method = "tick",
+            at = @At(value = "FIELD", target = "Lnet/minecraft/world/level/block/Blocks;GRASS_BLOCK:Lnet/minecraft/world/level/block/Block;"),
+            locals = LocalCapture.NO_CAPTURE
+    )
+    private void regions_unexplored$tick(CallbackInfo ci) {
+        BlockPos downPos = this.mob.blockPosition().below();
+        BlockState down = this.level.getBlockState(downPos);
+
+        // Modded Dirt likes
+        if (down.is(RuBlocks.PEAT_GRASS_BLOCK.get())) {
+            if (this.level.getGameRules().getBoolean(GameRules.RULE_MOBGRIEFING)) {
+                this.level.levelEvent(2001, downPos, Block.getId(RuBlocks.PEAT_GRASS_BLOCK.get().defaultBlockState()));
+
+                Block replacement = RuBlocks.PEAT_DIRT.get();
+
+                this.level.setBlock(downPos, replacement.defaultBlockState(), 2);
             }
+
+            this.mob.ate();
+        }
+
+        if (down.is(RuBlocks.SILT_GRASS_BLOCK.get())) {
+            if (this.level.getGameRules().getBoolean(GameRules.RULE_MOBGRIEFING)) {
+                this.level.levelEvent(2001, downPos, Block.getId(RuBlocks.SILT_GRASS_BLOCK.get().defaultBlockState()));
+
+                Block replacement = RuBlocks.SILT_DIRT.get();
+
+                this.level.setBlock(downPos, replacement.defaultBlockState(), 2);
+            }
+            this.mob.ate();
         }
     }
 }
